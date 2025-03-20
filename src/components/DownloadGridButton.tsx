6a5ef12c4f3f38ce1c1c347a52e7dc91438ad600
Mateo <mateo@voicelayer.ai>
2025-03-20 11:17:39 -0400

    remove unused

--- src/components/DownloadGridButton.tsx
+++ src/components/DownloadGridButton.tsx
@@ -4,7 +4,6 @@ export default function DownloadGridButton() {
   const { squareSize, grid } = useTejido()
 
   const handleDownload = () => {
-    // Convert the grid object to a JSON string
     const gridJSON = JSON.stringify(
       {
         ...grid,
@@ -13,22 +12,15 @@ export default function DownloadGridButton() {
       null,
       2
     )
-    // Create a blob with the JSON data
     const blob = new Blob([gridJSON], { type: "application/json" })
-
-    // Create a URL for the blob
     const url = URL.createObjectURL(blob)
-
-    // Create a temporary anchor element
     const a = document.createElement("a")
     a.href = url
     a.download = "tejido-grid.json"
 
-    // Trigger the download
     document.body.appendChild(a)
     a.click()
 
-    // Clean up
     document.body.removeChild(a)
     URL.revokeObjectURL(url)
   }
